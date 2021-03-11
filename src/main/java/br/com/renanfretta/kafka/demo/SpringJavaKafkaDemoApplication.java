83e1e5e6f52c69c879f51d92deefdc4e7b025c2b
renanfretta <renanfretta@vhintranet.com.br>
2021-03-11 12:55:21 -0300

    Criação da estrutura básica do projeto

---
+++ src/main/java/br/com/renanfretta/kafka/demo/SpringJavaKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.renanfretta.kafka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaKafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaKafkaDemoApplication.class, args);
+	}
+
+}
